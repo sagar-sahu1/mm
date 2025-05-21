f8f8b08b9e7df8f242cfa856dfdf7a38dc8ac43c
SahilSR81 <sahilrajputsingh8@gmail.com>
2025-05-21 13:33:37 +0530

    modified:   src/app/signup/loading.tsx

--- src/app/signup/loading.tsx
+++ src/app/signup/loading.tsx
@@ -4,13 +4,4 @@ export default function Loading() {
   return <LottieLoader text="Loading Signup..." />;
 } 
 
-import { LottieLoader } from '@/components/ui/LottieLoader';
-
-export default function Loading() {
-  return (
-    <div className="flex items-center justify-center min-h-[calc(100vh-theme(spacing.16))]">
-      <LottieLoader message="Loading..." size={120} />
-    </div>
-  );
-}
 
